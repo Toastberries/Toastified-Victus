f7a9c41dd1cc269a2e15f12f9c15b20dba0bb7e5
Toastberries <gerleig05@gmail.com>
2024-04-14 22:29:01 +0200

    Buffed Resurgence

--- src/main/java/com/glisco/victus/hearts/HeartAspectComponent.java
+++ src/main/java/com/glisco/victus/hearts/HeartAspectComponent.java
@@ -60,7 +60,7 @@ public class HeartAspectComponent implements Component, AutoSyncedComponent {
     }
 
     public void tick() {
-        final int rechargeRate = provider.hasStatusEffect(VictusStatusEffects.RESURGENCE) ? 2 + provider.getStatusEffect(VictusStatusEffects.RESURGENCE).getAmplifier() : 1;
+        final int rechargeRate = provider.hasStatusEffect(VictusStatusEffects.RESURGENCE) ? (2 + provider.getStatusEffect(VictusStatusEffects.RESURGENCE).getAmplifier())*5 : 1;
 
         for (int i = 0; i < effectiveSize(); i++) {
             HeartAspect aspect = this.aspects.get(i);
